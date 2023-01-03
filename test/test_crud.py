c296b71da46c4c05ba71b994185cf1d614fba101
Robert Walters <waltersmac@gmail.com>
2023-01-03 19:10:26 +0000

    Rearranging methods

--- test/test_crud.py
+++ test/test_crud.py
@@ -2,9 +2,6 @@ import unittest
 from core.crud import get_user_by_email, create_user, get_users, get_user, create_user_task, get_tasks
 
 class TestCrud(unittest.TestCase):
-    def test_get_user_by_email(self):
-        self.assertEqual(get_user_by_email(), None)
-
     def test_create_user(self):
         self.assertEqual(create_user(), None)
 
@@ -13,6 +10,9 @@ class TestCrud(unittest.TestCase):
 
     def test_get_user(self):
         self.assertEqual(get_user(), None)
+
+    def test_get_user_by_email(self):
+        self.assertEqual(get_user_by_email(), None)
 
     def test_create_user_task(self):
         self.assertEqual(create_user_task(), None)
